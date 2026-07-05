01599c7e0734ed86eb5081208be50d0c1d5f8d6d
Artem <artem.sereda.11@gmail.com>
2026-07-05 18:36:29 +0200

    Add soft delete class

--- backend/app/routers/classes.py
+++ backend/app/routers/classes.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/classes", tags=["classes"])
 
 @router.get("/", response_model=list[ClassResponse])
 async def get_lesson(db: AsyncSession = Depends(get_db)):
-    statement = select(ClassModel)
+    statement = select(ClassModel).where(ClassModel.is_active == True)
     result = await db.execute(statement)
     lessons = result.scalars()
     return lessons.all()
@@ -46,6 +46,20 @@ async def update_lesson(class_id: int, class_data: ClassUpdate, db: AsyncSession
     for key, value in class_data.model_dump(exclude_unset=True).items():
         setattr(lesson, key, value)
 
+    await db.commit()
+    await db.refresh(lesson)
+    return lesson
+
+@router.delete("/{class_id}", response_model=ClassResponse)
+async def delete_lesson(class_id: int, db: AsyncSession = Depends(get_db), current_admin: User = Depends(get_current_admin)):
+    statement = select(ClassModel).where(ClassModel.id == class_id)
+    result = await db.execute(statement)
+    lesson = result.scalar_one_or_none()
+
+    if not lesson:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    lesson.is_active = False
     await db.commit()
     await db.refresh(lesson)
     return lesson
